6c2aaa7fa304ee92caf0f01c4ea56b8a73190fed
AleksNau <workciti@yandex.ru>
2024-04-23 14:28:17 +0300

    данные

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 
 const {api} = require("./src/api/MainApi.js");
 const { SANDBOX_TOKEN  } = process.env;
+let myMoney = 0;
 
-api.getWithdrawLimits().then(res => console.log(res)).catch(res => console.log(res))
+const getMoney = () => {
+  api.getWithdrawLimits().then(res => {
+    myMoney = res.money[0].units;
+  }).catch(res => console.log(res))
+}
 
+const buyShare = () => {
 
+}
+
+const sellShare = () => {
+
+}
 /*
 
 
